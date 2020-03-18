9157a640220574e74a127804cfa4f7749edef348
bobiOne <bobione@mail.bg>
2020-03-18 12:47:08 +0200

    Latest changes

--- Web/ForumSystem.Web.ViewModels/Categories/PostInCategoryViewModel.cs
+++ Web/ForumSystem.Web.ViewModels/Categories/PostInCategoryViewModel.cs
@@ -1,5 +1,9 @@
 ﻿namespace ForumSystem.Web.ViewModels.Categories
 {
+    using System;
+    using System.Net;
+    using System.Text.RegularExpressions;
+
     using ForumSystem.Data.Models;
     using ForumSystem.Services.Mapping;
 
@@ -10,5 +14,20 @@
         public string UserUserName { get; set; }
 
         public int CommentsCount { get; set; }
+
+        public string Content { get; set; }
+
+        public DateTime CreatedOn { get; set; }
+
+        public string ShortContent
+        {
+            get
+            {
+                var content = WebUtility.HtmlDecode(Regex.Replace(this.Content, @"<[^>]+>", string.Empty));
+                return content.Length > 300
+                        ? content.Substring(0, 300) + "..."
+                        : content;
+            }
+        }
     }
 }
