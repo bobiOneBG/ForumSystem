eaeaf66638895679bf52634b5859af057b5f7f6a
bobiOne <bobione@mail.bg>
2020-04-15 13:23:27 +0300

    Test only

---
+++ Tests/ForumSystem.Services.Tests/HomePageTests.cs
@@ -0,0 +1,26 @@
+﻿namespace ForumSystem.Services.Tests
+{
+    using ForumSystem.Web;
+    using Microsoft.AspNetCore.Mvc.Testing;
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Xunit;
+
+    public class HomePageTests
+    {
+        [Fact]
+        public async Task HomePageShouldHaveH1Title()
+        {
+            var serverFactory = new WebApplicationFactory<Startup>();
+
+            var client = serverFactory.CreateClient();
+
+            var response = await client.GetAsync("/");
+            var reaponseAsString = await response.Content.ReadAsStringAsync();
+
+            Assert.Contains("<h1",  reaponseAsString);
+        }
+    }
+}
